d91ded34ed1d246aed7bf213f949708e8297ea84
David Noveczky <david.noveczky@gmail.com>
2020-01-29 23:08:11 +0100

    one 'x' moves on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <thread>
+
+#include <unistd.h>
+#include <term.h>
+
+void clearScreen()
+{
+    if (!cur_term)
+    {
+        int result;
+        setupterm( NULL, STDOUT_FILENO, &result );
+        if (result <= 0) return;
+    }
+    
+    putp( tigetstr( "clear" ) );
+}
 
 static constexpr size_t g_mapWidth = 80;
 static constexpr size_t g_mapHeight = 25;
 
 using map_t = char[g_mapWidth][g_mapHeight];
+
+void printMap(const map_t& f_map)
+{
+    clearScreen();
+    for(int i = 0; i < g_mapHeight; ++i)
+    {
+        for(int j = 0; j < g_mapWidth; ++j)
+        {
+            std::cout << f_map[j][i];
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main()
 {
@@ -22,15 +52,18 @@ int main()
             fin >> std::ws;
         }
     }
-
-    for(int i = 0; i < g_mapHeight; ++i)
+    std::chrono::seconds sec(1);
+    
+    
+    for(int i = 0; i<10; ++i)
     {
-        for(int j = 0; j < g_mapWidth; ++j)
-        {
-            std::cout << map[j][i];
-        }
-        std::cout << std::endl;
+        map[10+i][10] = 'x';
+        map[10+i-1][10] = ' ';
+        printMap(map);
+        
+        std::this_thread::sleep_for(sec);
     }
     
+
     return 0;
 }
