8b2cbd85ea2588f00620fd212e41875871fff89a
David Noveczky <david.noveczky@gmail.com>
2020-01-29 22:43:05 +0100

    read the default map and print it

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <fstream>
+
+static constexpr size_t g_mapWidth = 80;
+static constexpr size_t g_mapHeight = 25;
+
+using map_t = char[g_mapWidth][g_mapHeight];
 
 int main()
 {
-	std::cout << "Let's start..." << std::endl;
-	return 0;
+    map_t map;
+    
+    std::fstream fin("/Users/dnoveczky/git/flatland-simulator/data/map", std::fstream::in);
+    if(fin.is_open())
+    {
+        for(int i = 0; i < g_mapHeight; ++i)
+        {
+            for(int j = 0; j < g_mapWidth; ++j)
+            {
+                fin >> std::noskipws >> map[j][i];
+            }
+            fin >> std::ws;
+        }
+    }
+
+    for(int i = 0; i < g_mapHeight; ++i)
+    {
+        for(int j = 0; j < g_mapWidth; ++j)
+        {
+            std::cout << map[j][i];
+        }
+        std::cout << std::endl;
+    }
+    
+    return 0;
 }
