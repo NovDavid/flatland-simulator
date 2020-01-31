31c8df6cc133ae0bef5d72252b7968990ddd1d2d
David Noveczky <david.noveczky@gmail.com>
2020-01-31 23:35:20 +0100

    car is now movable with fixed steps

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <fstream>
-#include <chrono>
-#include <thread>
 
 #include <unistd.h>
 #include <term.h>
+
+#include <termios.h>
+#include <stdio.h>
 
 void clearScreen()
 {
@@ -22,23 +23,79 @@ static constexpr size_t g_mapWidth = 80;
 static constexpr size_t g_mapHeight = 25;
 
 using map_t = char[g_mapWidth][g_mapHeight];
+struct pos
+{
+    size_t col;
+    size_t row;
+};
 
-void printMap(const map_t& f_map)
+
+void printMap(const map_t& f_map, const pos& car)
 {
     clearScreen();
     for(int i = 0; i < g_mapHeight; ++i)
     {
         for(int j = 0; j < g_mapWidth; ++j)
         {
-            std::cout << f_map[j][i];
+            if(i == car.row && j == car.col)
+                std::cout << 'x';
+            else
+                std::cout << f_map[j][i];
         }
         std::cout << std::endl;
     }
 }
 
+static struct termios old, current;
+
+/* Initialize new terminal i/o settings */
+void initTermios(int echo)
+{
+    tcgetattr(0, &old); /* grab old terminal i/o settings */
+    current = old; /* make new settings same as old settings */
+    current.c_lflag &= ~ICANON; /* disable buffered i/o */
+    if (echo) {
+        current.c_lflag |= ECHO; /* set echo mode */
+    } else {
+        current.c_lflag &= ~ECHO; /* set no echo mode */
+    }
+    tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
+}
+
+/* Restore old terminal i/o settings */
+void resetTermios(void)
+{
+    tcsetattr(0, TCSANOW, &old);
+}
+
+/* Read 1 character - echo defines echo mode */
+char getch_(int echo)
+{
+    char ch;
+    initTermios(echo);
+    ch = getchar();
+    resetTermios();
+    return ch;
+}
+
+/* Read 1 character without echo */
+char getch(void)
+{
+    return getch_(0);
+}
+
+/* Read 1 character with echo */
+char getche(void)
+{
+    return getch_(1);
+}
+
+
 int main()
 {
     map_t map;
+    
+    pos carPosition;
     
     std::fstream fin("/Users/dnoveczky/git/flatland-simulator/data/map", std::fstream::in);
     if(fin.is_open())
@@ -47,23 +104,51 @@ int main()
         {
             for(int j = 0; j < g_mapWidth; ++j)
             {
-                fin >> std::noskipws >> map[j][i];
+                char c;
+                fin >> std::noskipws >> c;
+                if('x' == c)
+                {
+                    carPosition = pos{static_cast<size_t>(j),static_cast<size_t>(i)};
+                    map[j][i] = ' ';
+                }
+                else
+                {
+                    map[j][i] = c;
+                }
+                
             }
             fin >> std::ws;
         }
     }
-    std::chrono::seconds sec(1);
+    fin.close();
     
+    printMap(map, carPosition);
     
-    for(int i = 0; i<10; ++i)
+    char c = 0;
+    
+    while(c != 'q')
     {
-        map[10+i][10] = 'x';
-        map[10+i-1][10] = ' ';
-        printMap(map);
-        
-        std::this_thread::sleep_for(sec);
+        c = getch();
+        switch (c)
+        {
+            case 'w':
+                carPosition.row -=1;
+                break;
+            case 'a':
+                carPosition.col -= 1;
+                break;
+            case 's':
+                carPosition.row += 1;
+                break;
+            case 'd':
+                carPosition.col += 1;
+                break;
+            default:
+                break;
+        }
+        printMap(map, carPosition);
     }
     
-
+    
     return 0;
 }
